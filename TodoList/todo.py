a9d5da412b3d95754680e9e3f79bf73dc25328a5
ryosms <ryo.sms@gmail.com>
2013-12-27 22:04:04 +0900

    Add create new task method

--- TodoList/todo.py
+++ TodoList/todo.py
@@ -1,5 +1,5 @@
 import sqlite3
-from bottle import route, run, template
+from bottle import route, run, template, request
 
 @route('/todo')
 def todo_list():
@@ -10,5 +10,26 @@ def todo_list():
     c.close()
     output = template('make_table', rows=result)
     return str(output)
+
+
+@route('/new', method='GET')
+def new_item():
+
+    if request.GET.get('save','').strip():
+
+        new = request.GET.get('task', '').strip()
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+
+        c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new,1))
+        new_id = c.lastrowid
+
+        conn.commit()
+        c.close()
+
+        return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id
+    else:
+        return template('new_task')
+
 
 run(debug=True, reloader=True)
