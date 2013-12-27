2b5e47fe030267120e2f0ec88575932ce37fa33c
ryosms <ryo.sms@gmail.com>
2013-12-27 21:43:08 +0900

    Add template
    format display using a template

--- TodoList/todo.py
+++ TodoList/todo.py
@@ -1,5 +1,5 @@
 import sqlite3
-from bottle import route, run
+from bottle import route, run, template
 
 @route('/todo')
 def todo_list():
@@ -7,6 +7,8 @@ def todo_list():
     c = conn.cursor()
     c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
     result = c.fetchall()
-    return str(result)
+    c.close()
+    output = template('make_table', rows=result)
+    return str(output)
 
 run(debug=True, reloader=True)
