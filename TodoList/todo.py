29b2928d2c7e7df8523eb21ebcbfb12f09f8b60d
ryosms <ryo.sms@gmail.com>
2013-12-27 22:11:31 +0900

    Add edit task method

--- TodoList/todo.py
+++ TodoList/todo.py
@@ -1,6 +1,7 @@
 import sqlite3
 from bottle import route, run, template, request
 
+
 @route('/todo')
 def todo_list():
     conn = sqlite3.connect('todo.db')
@@ -15,13 +16,13 @@ def todo_list():
 @route('/new', method='GET')
 def new_item():
 
-    if request.GET.get('save','').strip():
+    if request.GET.get('save', '').strip():
 
         new = request.GET.get('task', '').strip()
         conn = sqlite3.connect('todo.db')
         c = conn.cursor()
 
-        c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new,1))
+        c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new, 1))
         new_id = c.lastrowid
 
         conn.commit()
@@ -30,6 +31,33 @@ def new_item():
         return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id
     else:
         return template('new_task')
+
+
+@route('/edit/<no:int>', method='GET')
+def edit_item(no):
+
+    if request.GET.get('save', '').strip():
+        edit = request.GET.get('task', '').strip()
+        status = request.GET.get('status', '').strip()
+
+        if status == 'open':
+            status = 1
+        else:
+            status = 0
+
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+        c.execute("UPDATE todo SET task = ?, status = ? WHERE id LIKE ?", (edit, status, no))
+        conn.commit()
+
+        return '<p>The item number %s was successfully updated</p>' % no
+    else:
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+        c.execute("SELECT task FROM todo WHERE id LIKE ?", (str(no)))
+        cur_data = c.fetchone()
+
+        return template('edit_task', old=cur_data, no=no)
 
 
 run(debug=True, reloader=True)
